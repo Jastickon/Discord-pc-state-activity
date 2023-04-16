a84fd22cf4e075c0023f40338ddf1da0965efd28
Kirill <128633248+Jastickon@users.noreply.github.com>
2023-04-16 17:51:12 +0300

    Add files via upload

---
+++ statusDIS.py
@@ -0,0 +1,17 @@
+from pypresence import Presence
+import time
+import psutil
+
+time1 = time.ctime()
+client_id = 1097099246328631337
+RPC = Presence(client_id)
+
+RPC.connect()
+while True:
+    time.sleep(1)
+    ram1=psutil.virtual_memory().percent
+    cpu1=psutil.cpu_percent()
+    ram2= psutil.swap_memory()
+    RPC.update(details= f"RAM: {ram1}% CPU: {cpu1}%", 
+            large_image='https://media.tenor.com/sKSw-rqsFxQAAAAi/foxy-foxplushy.gif',  
+)
