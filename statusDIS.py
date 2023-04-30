610feb7e5e80c84d2c458df493e770814e5b0d4b
Kirill <128633248+Jastickon@users.noreply.github.com>
2023-04-30 14:31:44 +0200

    Update statusDIS.py

--- statusDIS.py
+++ statusDIS.py
@@ -2,9 +2,21 @@ from pypresence import Presence
 import time
 import psutil
 
-time1 = time.ctime()
-client_id = id бота дискорда
+time.sleep(1)
+
+
+client_id = id Бота дискорда
 RPC = Presence(client_id)
+ButtonsList= [
+                {
+                    "label": "My Site",
+                    "url": "https://ametrin-smp.ru"
+                },
+                {
+                    "label": "GitHub",
+                    "url": "https://github.com/Jastickon"
+                }
+            ]
 
 RPC.connect()
 while True:
@@ -12,6 +24,9 @@ while True:
     ram1=psutil.virtual_memory().percent
     cpu1=psutil.cpu_percent()
     ram2= psutil.swap_memory()
-    RPC.update(details= f"RAM: {ram1}% CPU: {cpu1}%", 
-            large_image='https://media.tenor.com/sKSw-rqsFxQAAAAi/foxy-foxplushy.gif',  
+    RPC.update(details= f"RAM: {ram1}% CPU: {cpu1}%",   
+            large_image='https://media.tenor.com/8znDLN7uJqwAAAAi/b-eat-saber.gif',
+            large_text='3.14',
+            buttons=ButtonsList,
+            
 )
