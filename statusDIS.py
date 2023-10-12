3aa6a0ea306702db879f40b2ef587b024b819549
3.14 <128633248+Jastickon@users.noreply.github.com>
2023-10-12 21:17:57 +0200

    Добавил защиту от краша скрипта при выключении дискорда

--- statusDIS.py
+++ statusDIS.py
@@ -1,23 +1,32 @@
 from pypresence import Presence
+import pypresence
 import time
 import psutil
 import pynvml
-
-time.sleep(1)
+import random
 
 pynvml.nvmlInit()
 
-client_id = айди бота сюда
+client_id = 
 #Пример client_id = 1107349645656588412
 RPC: Presence = Presence(client_id)
 ButtonsList= [
                 {
-                    "label": "GitHub",
-                    "url": "https://github.com/Jastickon"
+                    "label": "Vizo Studio",
+                    "url": "https://discord.gg/zuuwz2bUmQ"
+                },
+                {
+                    "label": "Subterrania project",
+                    "url": "https://discord.gg/Hh5XAebxHu"
                 }
             ]
-
-RPC.connect()
+try:
+    RPC.connect()
+    error = 0
+except pypresence.exceptions.DiscordNotFound:
+    print("Жду запуска дисорда")
+    time.sleep(1)
+    error = 1
 
 device_count = pynvml.nvmlDeviceGetCount()
 if device_count == 0:
@@ -26,8 +35,14 @@ if device_count == 0:
 else:
     has_video_card = True
 
+proc = 0
+text = "Loading..."
 while True:
     time.sleep(1)
+
+    rasudok = random.randint(0, 30)
+
+
     ram_percent=psutil.virtual_memory().percent
     cpu_percent=psutil.cpu_percent()
     
@@ -39,12 +54,78 @@ while True:
             gpu_utilization += f"{pynvml.nvmlDeviceGetUtilizationRates(handle).gpu / 1:.1f}% "
     else:
         gpu_utilization = ''
- # здесь отоброжение в приложение
-    RPC.update(details=f"CPU: {cpu_percent}% GPU: {gpu_utilization} RAM: {ram_percent}%",
-            large_image='https://media.tenor.com/8znDLN7uJqwAAAAi/b-eat-saber.gif',
-            large_text='3.14',
-            buttons=ButtonsList,
-            small_image='https://cdn3.emoji.gg/emojis/8235-streaming.png',
-    )
-    
+
+    proc += 1
+
+    if proc == 1:
+        text = "10% [■_________]"
+    elif proc == 2:
+        text = "20% [■■________]"
+    elif proc == 3:
+        text = "30% [■■■_______]"
+    elif proc == 4:
+        text = "40% [■■■■______]"
+    elif proc == 5:
+        text = "50% [■■■■■_____]"
+    elif proc == 6:
+        text = "60% [■■■■■■____]"
+    elif proc == 7:
+        text = "70% [■■■■■■■___]"
+    elif proc == 8:
+        text = "80% [■■■■■■■■__]"
+    elif proc == 9:
+        text = "90% [■■■■■■■■■_]"
+    elif proc == 10:
+        text = "100% [■■■■■■■■■■]"
+    elif proc >= 11:
+        if proc == 12:
+            text = "{}"
+        elif proc == 13:
+            text = "{=}"
+        elif proc == 14:
+            text = "{==}"
+        elif proc == 15:
+            text = "{===}"
+        elif proc == 16:
+            text = "{====}"
+        elif proc == 17:
+            text = "{𝙊𝙥𝙚𝙣}"
+        elif proc > 17:
+            text = f"Возраст: 𝐍𝐨𝐧𝐞  Рассудок: {rasudok}"
+    if proc > 37:
+        proc = 0
+        text = "Loading..."
+
+    try:
+        if error == 1:
+            try:
+                try:
+                    RPC.connect()
+                    error = 0
+                except pypresence.exceptions.DiscordError:
+                    pass
+            except pypresence.exceptions.DiscordNotFound:
+                error = 1
+        if error == 0:
+            try:
+                # здесь отоброжение в приложение
+                RPC.update(details=f"𝐂𝐏𝐔: {cpu_percent}% 𝐆𝐏𝐔: {gpu_utilization} 𝐑𝐀𝐌: {ram_percent}%",
+                           state=text,
+                           large_image='https://i.pinimg.com/originals/bf/2a/85/bf2a85f922ef8589dd4ccc180edb3e30.gif',
+                           large_text='Кирилл',
+                           buttons=ButtonsList,
+                           small_image='https://cdn.discordapp.com/emojis/790045045813542912.gif?size=96&quality=lossless',
+                           small_text='𝐕𝐞𝐫𝐢𝐟𝐢𝐞𝐝'
+                           )
+            except pypresence.exceptions.PipeClosed:
+                print("Дискорд закрыт")
+                RPC.close()
+                error = 1
+                time.sleep(1)
+
+    except pypresence.exceptions.DiscordNotFound:
+        RPC.close()
+        error = 1
+
+
 pynvml.nvmlShutdown()
